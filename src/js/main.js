8b88425bb18173691c915146b7c9beba0e7dbc68
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-01-07 22:48:22 +0300

    rrrrrr

--- src/js/main.js
+++ src/js/main.js
@@ -25,40 +25,42 @@ function averageСost(arr) {
   let cyl = new Cylinder(2, 1) 
   console.log("volume:", cyl.volume().toFixed(2) + ", " + "area:", cyl.area().toFixed(2));
  
-// ЗАДАЧА 3 :(
+// ЗАДАЧА 3 
   function odd(start, finish){
+  let result = "";
   for (let i = start; i <= finish; i++)
    {
-    if (i % 2 !== 0);
-        {
-      console.log(i);
+    if (i % 2 !== 0) {
+      result = result + " " + (i); {
+      }
     }
-  }
-  odd (2,7)
+   }
+   return result;
 }
+const str = odd (2,7);
+console.log(str);
+
 //ЗАДАЧА 4
 function reverseString(str) { 
 return str.split("").reverse().join("");
 };
 console.log(reverseString("abc")); 
-// ЗАДАЧА 5 :(
-let arr = ['madam was looking on the radar for a kaya']
-const palindrome = (arr) => {
-  let count = 0
-  for(let key of arr){
-    if(key.length > 5 || key.length < 5){
-      continue
-    }
-    if(parseInt(key).toString().length != key.length){
-      continue
-    }
-    if(+key == +key.split('').reverse().join('')){
-      count++
-    }
-  }
-  return count
-}
-let rezult = palindrome(arr) 
-console.log(result)
-console.log(palindrome(arr)) 
 
+// ЗАДАЧА 5
+function palindromes(str) {
+let arr = str.split(' ');
+let result = '';
+for (let i = 0; i < arr.length; i++) {
+let pal = arr[i];
+function reverse(pal) {
+return pal.split("").reverse().join("");
+}
+const palReverse = reverse(pal);
+if(pal === palReverse)
+if (pal.length >= 2) {
+ result = result + pal + " "; 
+}
+}
+return result;
+}
+console.log(palindromes('madam was looking on the radar for a kayak'));

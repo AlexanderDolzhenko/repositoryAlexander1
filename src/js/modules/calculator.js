9d9b1759f11d9fc1ee2d99ef3fc181af713c8ccd
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-01-15 12:52:58 +0300

    bbb

--- src/js/modules/calculator.js
+++ src/js/modules/calculator.js
@@ -8,22 +8,33 @@ export class Calculator {
         const symbolStr = String(symbol);
 
         if(!VALID_SYMBOLS.test(symbolStr)) {
-            return this.#getCurrentString();
+            return this.getCurrentString();
         }
         if (ACTIONS.test(symbol) && !this.#secondNumber) {
             this.#action = symbol;
-            return this.#getCurrentString();
+            return this.getCurrentString();
         }
         if (!ACTIONS.test(symbol) && !this.#action) {
             this.#firstNumber += symbol;
-            return this.#getCurrentString();
+            return this.getCurrentString();
         } 
         if (!ACTIONS.test(symbol) && this.#action) {
             this.#secondNumber += symbol;
-            return this.#getCurrentString();
+            return this.getCurrentString();
         } 
     }
-    #getCurrentString() {
+    getCurrentString() {
         return `${this.#firstNumber} ${this.#action} ${this.#secondNumber}`
+    }
+    calculate() {
+        const leftOperand = +this.#firstNumber;
+        const rightOperand = +this.#secondNumber;
+const actions = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+}
+        return actions[this.#action](leftOperand, rightOperand); 
     }
 }
--- src/js/practice.js
+++ src/js/practice.js
@@ -3,4 +3,7 @@ import { Calculator } from "./modules/calculator.js";
 const calculator = new Calculator();
 console.log(calculator.setSign('1'));
 console.log(calculator.setSign('5'));
-console.log(calculator.setSign('d'));
+console.log(calculator.setSign('0'));
+console.log(calculator.setSign('+'));
+console.log(calculator.setSign('10'));
+console.log(calculator.calculate());

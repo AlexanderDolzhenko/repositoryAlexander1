ff0a8d81c06d97124268a59654873ac225e069ba
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-01-25 17:47:00 +0300

    fff

--- src/js/practice.js
+++ src/js/practice.js
@@ -1,9 +1,27 @@
 export function getCard({name, imgPath, price, count}) {
     const card = document.createElement('div');
     card.innerHTML = `${name}`;
-    
-    
+    card.classList.add('cardgood');
+
+    const pictures = document.createElement('img');
+    pictures.src = `${imgPath}`;
+    card.appendChild(pictures);
+
+    const prices = document.createElement('div');
+    prices.classList.add('cardprice');
+    prices.innerHTML = `${price}`;
+    card.appendChild(prices);
+
+    const stockCount = document.createElement('input');
+    stockCount.classList.add('stocknumber');
+    // place for input type
+    stockCount.innerHTML = `${count}`;
+    card.appendChild(stockCount);
+
     return card
 }
 
 
+const card = getCard({name: 'Milky way'}, {imgPath: '/img/milky.jpg'}, {price: '2$'}, {count: '2'});
+
+document.body.appendChild(card);

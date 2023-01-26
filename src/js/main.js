0b7ebfa21515cb822caa0cdc33a14a1a08adc0bb
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-01-26 21:25:02 +0300

    TTTT

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import { getCard } from "./modules/practice.js";
 const card = getCard({
     name: 'Milky way', 
     imgPath: '/img/milky.jpg', 
-    price: '2$',
+    price: '2 $',
     count: '2'});
     
     document.body.appendChild(card);
--- src/js/modules/practice.js
+++ src/js/modules/practice.js
@@ -1,23 +1,40 @@
+
 function getCard({name, imgPath, price, count}) {
     const card = document.createElement('div');
-    card.innerHTML = `${name}`;
-    card.classList.add('cardgood');
+    card.classList.add('goodsCard')
+
+    const cardhead = document.createElement('div');
+    cardhead.innerText = `${name}`;
+    cardhead.classList.add('cardHeader');
+    card.appendChild(cardhead);
+
+    const cardImg = document.createElement('div');
+    cardImg.classList.add('cardImg');
+    card.appendChild(cardImg);
+
+    const rate = document.createElement('div');
+    rate.classList.add('rateGood');
+    rate.innerText = '';
+    card.appendChild(rate);
 
     const pictures = document.createElement('img');
-    pictures.classList.add('imggood');
+    pictures.classList.add('imgGood');
     pictures.src = `${imgPath}`;
-    card.appendChild(pictures);
+    cardImg.appendChild(pictures);
+
+    const cardfooter = document.createElement('div');
+    cardfooter.classList.add('cardFooter');
+    card.appendChild(cardfooter);
 
     const prices = document.createElement('div');
-    prices.classList.add('cardprice');
-    prices.innerHTML = `${price}`;
-    card.appendChild(prices);
+    prices.classList.add('cardPrice');
+    prices.innerText = `${price}`;
+    cardfooter.appendChild(prices);
 
-    const stockCount = document.createElement('input');
-    stockCount.classList.add('stocknumber');
-    // place for input type
-    stockCount.innerHTML = `${count}`;
-    card.appendChild(stockCount);
+    const stockCount = document.createElement('div');
+    stockCount.classList.add('stockNumber');
+    stockCount.innerText = `${count}`;
+    cardfooter.appendChild(stockCount);
 
     return card
 }
@@ -25,3 +42,46 @@ function getCard({name, imgPath, price, count}) {
 export {getCard};
 
 
+/* Альтернатива, очень продвинутая
+
+function getElement(tag, innerText, attributes) {
+    const el = document.createElement(tag);
+    Object.keys(attributes).forEach(attribute => {
+      el[attribute] = attributes[attribute]
+    })
+    el.innerText = innerText || '';
+    
+    return el
+  }
+  function getCard({name, imgPath, price, count}) {
+    const card = getElement('div','' , {className: 'card'} );
+
+    const img = getElement('img', '', {src: imgPath, className: 'card__img'} );
+    card.appendChild(img);
+
+    const footer = document.createElement('div');
+    footer.classList.add('card__footer');
+
+    const priceEl = document.createElement('div');
+    priceEl.classList.add('card__price');
+    priceEl.innerText = price;
+
+    const countEl = document.createElement('div');
+    countEl.classList.add('card__count');
+    countEl.innerText = count;
+
+    const nameEl = document.createElement('div');
+    nameEl.classList.add('card__name');
+    nameEl.innerText = name;
+
+    const inputEl = document.createElement('input');
+    inputEl.type = 'number';
+    inputEl.value = 2;
+    inputEl.name = 'input-bbbb';
+    inputEl.addEventListener('change', ({target: {value}}) => console.log(value))
+
+    footer.append(priceEl, countEl, inputEl);
+    card.appendChild(nameEl);
+    card.appendChild(footer);
+    return card;
+  } */

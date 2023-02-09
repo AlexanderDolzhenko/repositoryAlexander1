0b15067e00a1a2f17458ff1da88ae91e0655f723
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-02-09 15:50:21 +0300

    xcxcxc

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import View from "./modules/view.js";
-import {Calculator} from "./modules/calculator.js";
+import { Calculator } from "./modules/calculator.js";
 import Controller from "./modules/controller.js";
 
 const buttons = ['+', '-', '*', '/'];
@@ -30,7 +30,7 @@ class Veichle {
         }
         this.#vin = vin;
     }
-    
+
     get color() {
         return this.#color;
     }
@@ -42,67 +42,67 @@ class Veichle {
     }
 }
 class Truck extends Veichle {
-#loadcapacity;
-constructor(vin, color, loadCapacity) {
-    super(vin, color);
-    this.#loadcapacity = loadCapacity;
-}
-get loadcapacity() {
-    return this.#loadcapacity;
-}
-set loadcapacity(loadcapacity) {
-    if (typeof loadcapacity !== 'number') {
-        throw new Error('loadcapacity should be a number');
+    #loadcapacity;
+    constructor(vin, color, loadCapacity) {
+        super(vin, color);
+        this.#loadcapacity = loadCapacity;
     }
-    this.#loadcapacity = loadcapacity;
-}
+    get loadcapacity() {
+        return this.#loadcapacity;
+    }
+    set loadcapacity(loadcapacity) {
+        if (typeof loadcapacity !== 'number') {
+            throw new Error('loadcapacity should be a number');
+        }
+        this.#loadcapacity = loadcapacity;
+    }
 }
 class Bus extends Veichle {
-#sitsCapacity;
-constructor(vin, color, sitsCapacity) {
-    super(vin, color);
-    this.#sitsCapacity = sitsCapacity;
-}
-get sitsCapacity() {
-    return this.#sitsCapacity;
-}
-set sitsCapacity(sitsCapacity) {
-    if (typeof sitsCapacity !== 'number') {
-        throw new Error('sitsCapacity should be a number');
+    #sitsCapacity;
+    constructor(vin, color, sitsCapacity) {
+        super(vin, color);
+        this.#sitsCapacity = sitsCapacity;
     }
-    this.#sitsCapacity = sitsCapacity;
-}
+    get sitsCapacity() {
+        return this.#sitsCapacity;
+    }
+    set sitsCapacity(sitsCapacity) {
+        if (typeof sitsCapacity !== 'number') {
+            throw new Error('sitsCapacity should be a number');
+        }
+        this.#sitsCapacity = sitsCapacity;
+    }
 }
 
 class Dealer {
-#title;
-#veicles;
-constructor(title, veicles) {
-    this.#title = title;
-    this.#veicles = veicles;
-}
-get title() {
-    return this.#title;
-}
-set title(title) {
-    if (typeof title !== 'string') {
-        throw new Error('title should be a string');
+    #title;
+    #veicles;
+    constructor(title, veicles) {
+        this.#title = title;
+        this.#veicles = veicles;
     }
-    this.#title = title;
-}
-get veicles() {
-    return this.#veicles;
-}
-set veicles(veicles) {
-    if (typeof veicles !== new Veichle) {
-        throw new Error('veicles should be a massive');
+    get title() {
+        return this.#title;
     }
-    this.#veicles = veicles;
-}
-addVeichle(Veichle){};
+    set title(title) {
+        if (typeof title !== 'string') {
+            throw new Error('title should be a string');
+        }
+        this.#title = title;
+    }
+    get veicles() {
+        return this.#veicles;
+    }
+    set veicles(veicles) {
+        if (typeof veicles !== new Veichle) {
+            throw new Error('veicles should be a massive');
+        }
+        this.#veicles = veicles;
+    }
+    addVeichle(Veichle) { };
 
-sellVeichle(vin){};
-repaintVeichle(vin){};
+    sellVeichle(vin) { };
+    repaintVeichle(vin) { };
 
 }
 const divan = new Dealer;
@@ -110,37 +110,37 @@ console.log(divan);
 
 const DATABASE = {
     dealer: {
-      title: "Trucks & Buses",
+        title: "Trucks & Buses",
     },
     trucks: [
-      {
-        vin: 1112,
-        color: "Red",
-        loadCapacity: 10,
-      },
-      {
-        vin: 2332,
-        color: "Yellow",
-        loadCapacity: 20,
-      },
-      {
-        vin: 5234,
-        color: "Green",
-        loadCapacity: 70,
-      },
+        {
+            vin: 1112,
+            color: "Red",
+            loadCapacity: 10,
+        },
+        {
+            vin: 2332,
+            color: "Yellow",
+            loadCapacity: 20,
+        },
+        {
+            vin: 5234,
+            color: "Green",
+            loadCapacity: 70,
+        },
     ],
     buses: [
-      {
-        vin: 1112,
-        color: "Green",
-        sitsCapacity : 50,
-      },
-      {
-        vin: 6543,
-        color: "Yellow",
-        sitsCapacity : 25,
-      },
+        {
+            vin: 1112,
+            color: "Green",
+            sitsCapacity: 50,
+        },
+        {
+            vin: 6543,
+            color: "Yellow",
+            sitsCapacity: 25,
+        },
     ],
-  };
-  let dealer = DATABASE;
+};
+let dealer = DATABASE;
 console.log(dealer);

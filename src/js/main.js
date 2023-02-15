455833955d25b0d21337dcb6aa5231b4484adaa7
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-02-15 18:44:45 +0300

    ccc

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
 //TASK 1
+const prom1 = Promise.resolve(1);
+const prom2 = Promise.resolve(2);
+
 function all(array) {
-    array = [];
-    const prom1 = new Promise((resolve, reject) => {
-        let a = 1;
-        resolve (a);
-    });
-    const prom2 = new Promise((resolve, reject) => {
-       let b = 2;
-        resolve (b);
-    });    
+    return Promise.all(array);
 }
 all([prom1, prom2]).then(([a, b]) => {
     console.log(a, b);   
 });
+
+//TASK 2
+

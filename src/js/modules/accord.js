6fdfa55f197d78846040f5279474bb081361c5c7
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-03-01 18:51:30 +0300

    houou

--- src/js/modules/accord.js
+++ src/js/modules/accord.js
@@ -1,16 +1,16 @@
-export function accordion () {
+export function accordion() {
     let acc = document.getElementsByClassName("accordion");
     let i;
-    
+
     for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      });
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
     }
 }

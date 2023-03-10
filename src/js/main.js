982511778fdca2906694408777f41450152f8075
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-03-10 21:41:45 +0300

    cvcvc

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,5 @@ import { Slider } from "./modules/gallery.js";
 Slider();
 import { Burgermenu } from "./modules/burgerMenu.js";
 Burgermenu();
+import { StickyMenu } from "./modules/stickyMenu.js";
+StickyMenu();

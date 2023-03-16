8706ed0df9d37cb529425f431a8319541c7d5ddf
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-03-16 22:06:30 +0300

    Modal POST

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,20 @@ import { scrollToPage } from "./modules/scrollTo.js";
 scrollToPage()
 import { remainingTimer } from "./modules/timer.js";
 remainingTimer();
-import { modalWindow } from "./modules/modal.js";
+import { modalWindow } from "./modules/modal_start.js";
 modalWindow();
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7jjWSj0RNo7LktE73M9149bl4WQSDN9I",
+  authDomain: "js-diploma-test.firebaseapp.com",
+  databaseURL: "https://js-diploma-test-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "js-diploma-test",
+  storageBucket: "js-diploma-test.appspot.com",
+  messagingSenderId: "697670536737",
+  appId: "1:697670536737:web:9d1f2ef442e4095822dd60"
+};
+
+

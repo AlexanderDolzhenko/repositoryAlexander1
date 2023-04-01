f1401efc69c2af94dda8cd1fbe9a71df61237e55
AlexanderDolzhenko <dolzhenkoa93@gmail.com>
2023-04-01 11:38:24 +0300

    Final commit

--- src/js/main.js
+++ src/js/main.js
@@ -116,6 +116,15 @@ function showThanksModal(message) {
 };
 
 
+$('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
 
 
 

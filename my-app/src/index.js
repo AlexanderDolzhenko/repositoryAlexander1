c8cf9b0495b1a6d099c287bf92ccf59a0112c05a
Alexander <dolzhenkoa93@gmail.com>
2023-04-04 00:06:40 +0300

    fff

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const elem = (
+  <div>
+    <h2>Hello World</h2>
+    <input type = "text"/>
+    <button>Click</button>
+  </div>
+)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ elem
 );
 
 // If you want to start measuring performance in your app, pass a function

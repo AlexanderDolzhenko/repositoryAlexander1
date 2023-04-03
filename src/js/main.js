9be2d82aa4b9e589d2597221bc34ba89fbae52e4
Alexander <dolzhenkoa93@gmail.com>
2023-04-03 16:33:58 +0300

    FFF

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import { getCard } from "./modules/practice.js";
-const card = getCard({
-    name: 'Milky way', 
-    imgPath: '/img/milky.jpg', 
-    price: '2 $',
-    count: '2'});
-    
-    document.body.appendChild(card);
